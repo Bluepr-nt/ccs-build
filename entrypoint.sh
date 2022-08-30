7b1cbb04e16e1e753cd112af938fdb3a4851bff8
Vincent Dansereau <dansereauvincent@gmail.com>
2022-08-30 08:54:14 -0400

    feat(action): WIP base implementation of calling and action -- add debug info

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/bash -x
+#!/usr/bin/bash
 
-echo $ENV
+echo ENV: $ENV
 if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
 fi
 
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" \
--v $GITHUB_WORKSPACE :/var/www --network $JOB_CONTAINER_NETWORK  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
+# exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $GITHUB_WORKSPACE :/var/www --network $JOB_CONTAINER_NETWORK  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
