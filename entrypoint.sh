556a3f8f0079aba4c1f037ae37f8b49ed9d8c21b
Vincent Dansereau <dansereauvincent@gmail.com>
2022-08-30 09:11:52 -0400

    feat(action): fix  entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --pa
 fi
 
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $GITHUB_WORKSPACE:/var/www --network $INPUT_NETWORK  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $GITHUB_WORKSPACE:/var/www --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
