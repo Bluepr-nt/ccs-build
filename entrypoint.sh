b9c020f51d210fa8e9132ad50fec591491cf41eb
Vincent Dansereau <dansereauvincent@gmail.com>
2022-08-30 09:07:09 -0400

    feat(action): WIP base implementation of calling and action -- reactivate docker exec in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --pa
 fi
 
 
-# exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $GITHUB_WORKSPACE :/var/www --network $JOB_CONTAINER_NETWORK  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $GITHUB_WORKSPACE :/var/www --network $JOB_CONTAINER_NETWORK  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
