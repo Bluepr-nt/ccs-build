d3c8805f47edb728ac9379e008018e2527593609
Vincent Dansereau <dansereauvincent@gmail.com>
2022-08-29 22:49:25 -0400

    feat(action): WIP base implementation of calling and action

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo $ENV
+if [ ! -z $INPUT_USERNAME ];
+then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+fi
+
+
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" \
+-v ${{ GITHUB_WORKSPACE }}:/var/www --network $JOB_CONTAINER_NETWORK  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
