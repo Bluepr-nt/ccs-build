5f41a8dc2104c31078aec3e1bb6d86ffdf448618
Vincent Dansereau <dansereauvincent@gmail.com>
2022-08-30 08:48:08 -0400

    feat(action): WIP base implementation of calling and action -- fix entrypoint.sh variable substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ fi
 
 
 exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" \
--v ${{ GITHUB_WORKSPACE }}:/var/www --network $JOB_CONTAINER_NETWORK  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
+-v $GITHUB_WORKSPACE :/var/www --network $JOB_CONTAINER_NETWORK  --entrypoint=sh $INPUT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
