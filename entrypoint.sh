ea0a4556835560dc22474c0ba9150517e37cf090
Vincent Dansereau <dansereauvincent@gmail.com>
2022-08-31 22:04:07 -0400

    test(pipeline_artifacts_test):  run test to compare with act results

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,5 @@ fi
 docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/var/www --entrypoint=$INPUT_SHELL $INPUT_ENVIRONMENT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
 echo "::set-output name=result_code::$?"
 echo "::set-output name=pipeline_artifacts::test"
+
 # Generate build_id
