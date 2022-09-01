25bc208c2295b91b8e0cfd4fd41b7de7c0c42718
Vincent Dansereau <dansereauvincent@gmail.com>
2022-08-31 20:09:08 -0400

    refactore(entrypoint.sh): wrap lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 if [ ! -z $INPUT_ENVIRONMENT_USERNAME ];
-then echo $INPUT_ENVIRONMENT_PASSWORD | docker login $INPUT_ENVIRONMENT_REGISTRY -u $INPUT_ENVIRONMENT_USERNAME --password-stdin
+then echo $INPUT_ENVIRONMENT_PASSWORD | docker login $INPUT_ENVIRONMENT_REGISTRY \
+  -u $INPUT_ENVIRONMENT_USERNAME --password-stdin
 fi
 
 
-docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/var/www --entrypoint=$INPUT_SHELL $INPUT_ENVIRONMENT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
+docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/var/www \
+  --entrypoint=$INPUT_SHELL $INPUT_ENVIRONMENT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
 echo "::set-output name=result_code::$?"
+# Generate build_id
