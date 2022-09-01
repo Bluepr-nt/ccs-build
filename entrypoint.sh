aec72bd90ec9c1aa66a917575b1508f4907ef923
Vincent Dansereau <dansereauvincent@gmail.com>
2022-09-01 09:27:23 -0400

    feat(action): Add configurable workdir

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ then echo $INPUT_ENVIRONMENT_PASSWORD | docker login $INPUT_ENVIRONMENT_REGISTRY
 fi
 
 
-docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:/var/www --entrypoint=$INPUT_SHELL $INPUT_ENVIRONMENT_IMAGE -c "${INPUT_COMMAND//$'\n'/;}"
+docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v $INPUT_WORKSPACE:$INPUT_ENVIRONMENT_WORKDIR -w $INPUT_ENVIRONMENT_WORKDIR --entrypoint=$INPUT_SHELL $INPUT_ENVIRONMENT_IMAGE -c "mkdir /var/www/${INPUT_COMMAND//$'\n'/;};RESULT_CODE=$?"
 echo "::set-output name=result_code::$?"
-echo "::set-output name=pipeline_artifacts::test"
+LIST=`ls $INPUT_WORKSPACE`
+echo "::set-output name=pipeline_artifacts::${LIST//$'\n'/, }"
 
 # Generate build_id
